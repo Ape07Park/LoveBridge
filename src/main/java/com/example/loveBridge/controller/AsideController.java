f353f62755735885b81d8faf8b4a8f1a429a8b88
Daniel Park <ape072399@gmail.com>
2024-04-01 19:17:09 +0900

    mainPage

--- src/main/java/com/example/loveBridge/controller/AsideController.java
+++ src/main/java/com/example/loveBridge/controller/AsideController.java
@@ -1,6 +1,6 @@
 package com.example.LoveBridge.controller;
 
-import java.util.Map;    
+import java.util.Map;     
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
